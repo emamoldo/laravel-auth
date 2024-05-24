6e6c0b02e04331536c6e0a138c133d1136788d66
emamoldo <moldovanemanuel92.em@gmail.com>
2024-05-24 14:00:42 +0200

    Add the project to the new repo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,7 +80,7 @@
                                     <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
                                     <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                            document.getElementById('logout-form').submit();">
+                                                document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
